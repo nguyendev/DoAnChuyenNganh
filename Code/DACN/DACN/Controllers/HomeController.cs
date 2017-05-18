611b1b58f0584bfcd0967efa2fd64e0bc9e8e2a6
nguyendev <nguyen.nah76@gmail.com>
2017-05-18 01:28:23 -0700

    1.0

---
+++ Code/DACN/DACN/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DACN.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<String> IndexAsync()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("MyClient", "1.0"));
+            var result = await httpClient.GetStringAsync("http://www.microsoft.com");
+
+            return result;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
